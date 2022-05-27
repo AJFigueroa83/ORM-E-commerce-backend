0610df36d3b9f267535015166c89c081801a80cc
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-05-26 23:11:49 -0400

    fixed error in code with the relationships which was not letting the get routes to work properly.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -19,14 +19,16 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
-    model: ProductTag
+    model: ProductTag,
+    foreignKey: 'product_id'
   }
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
-    model: ProductTag
+    model: ProductTag,
+    foreignKey: 'tag_id'
   }
 })
 
